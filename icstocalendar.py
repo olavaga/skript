9f07932a0002d4f4c56be996bd30aedbb09cd0d8
Olav Nicolay Larsson Aga <olavnlaga@gmail.com>
2020-09-02 10:48:30 +0200

    Fiksa konvertering til lokal tid

--- icstocalendar.py
+++ icstocalendar.py
@@ -1,6 +1,9 @@
 # Inspirert av https://stackoverflow.com/questions/3408097/parsing-files-ics-icalendar-using-python
 from icalendar import Calendar, vDatetime
 from datetime import datetime
+from pytz import timezone, utc
+
+localtime = timezone('Europe/Oslo')
 
 icsfile = input()
 g = open(icsfile,'r')
@@ -15,6 +18,9 @@ for component in gcal.walk():
         datebytes = vDatetime.to_ical(component['dtstart'])
         date = datebytes.decode("utf-8")
         dt = datetime.strptime(date, '%Y%m%dT%H%M%SZ')
+        dt = utc.localize(dt)
+        dt = dt.astimezone(localtime)
+        
         mmdd = str(dt.month) + '\\' + str(dt.day)
         hhmm = str(dt.hour) + ':' + str(dt.minute)
         print(mmdd, '\t', hhmm, summary, location)
